ef1185906993a45410bc468036c4741765416668
Bexultan Myrzatayev <youngdeveloper.co@gmail.com>
2021-06-15 17:25:41 +0600

    initial setup

---
+++ src/app/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Navbar = () => (
+  <nav>
+    <section>
+      <h1>Redux Essentials Example</h1>
+
+      <div className="navContent">
+        <div className="navLinks" />
+      </div>
+    </section>
+  </nav>
+);
